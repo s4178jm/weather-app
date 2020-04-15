e050363c0b769422eb4771ded91927d34798f38a
s4178jm <60681747+s4178jm@users.noreply.github.com>
2020-04-15 20:49:22 +0800

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function About() {
   return <div>
-    <h3>This is an app which show the weather in your area.<br/>
+    <h3>This is an app which shows the weather in your area.<br/>
     Just type in your zipcode and weather info would show up on the web.<br/>
     </h3>
   </div>;
